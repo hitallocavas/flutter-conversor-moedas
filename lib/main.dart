7482ca4525905259ffb3e38f134fb8a4dfcae47c
hitallocavas <hcs3@cin.ufpe.br>
2020-03-21 16:43:20 -0300

    Adicionando estilos para finalização de app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import "package:http/http.dart" as http;
 import 'dart:async';
@@ -8,6 +9,16 @@ const request = "https://api.hgbrasil.com/finance?format=json&key=9ed6b057";
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
@@ -22,6 +33,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  void clearAll(){
+    this.realController.clear();
+    this.dolarController.clear();
+    this.euroController.clear();
+  }
+
+  void changeReal(String text){
+    if(text.isEmpty){
+      clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real/this.dolar).toStringAsFixed(2);
+    euroController.text = (real/this.euro).toStringAsFixed(2);
+  }
+
+  void changeDolar(String text){
+    if(text.isEmpty){
+      clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (this.dolar * dolar).toStringAsFixed(2);
+    euroController.text = ((this.dolar * dolar)/euro).toStringAsFixed(2);
+  }
+
+  void changeEuro(String text){
+    if(text.isEmpty){
+      clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (this.euro * euro).toStringAsFixed(2);
+    dolarController.text = ((this.euro * euro)/this.dolar).toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,10 +108,26 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
                 return Center(
-                  child: Text(
-                    snapshot.data.toString()
-                  ),
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(
+                          Icons.monetization_on, color: Colors.amber, size: 150.0,
+                        ),
+                        Divider(),
+                        buildTextField("Reais", "R\$", realController, changeReal),
+                        Divider(),
+                        buildTextField("Dólares", "US\$", dolarController, changeDolar),
+                        Divider(),
+                        buildTextField("Euro", "€", euroController, changeEuro)
+                      ],
+                    ),
+                  )
                 );
               }
           }
@@ -65,4 +135,19 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
+
+Widget buildTextField(String currency, String symbol, TextEditingController controller, Function change) {
+  return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+        labelText: currency,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: symbol
+    ),
+    style: TextStyle(color: Colors.amber, fontSize: 20.0),
+    onChanged: change,
+    keyboardType: TextInputType.number,
+  );
 }
